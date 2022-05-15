05be89d40f5e1809547f4388cd8cb9c88c8b90b4
Diego Castro <df.castrop1@uniandes.edu.co>
2022-05-14 22:04:23 -0500

    Adapting other scenarios to screenshots

--- cypress/cypress/integration/ghost.spec.js
+++ cypress/cypress/integration/ghost.spec.js
@@ -8,6 +8,53 @@ describe('Ghost testing', () => {
        login(userName, password);
     })
 
+    it('Test scenario #1', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        updateAPost()
+        deleteAPost()
+        createAPost('A new post', 'A new description')
+        
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExist(postUrl)
+        })
+    });
+
+    it('Test scenario #2', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        deleteAPost()
+        logout()
+        login(userName, password)
+        createAPost('A new post')
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
+    it('Test scenario #3', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        deleteAPost(postTitle)
+        createAPost('A new post 2', 'A new description 2')
+        updateAPost()
+
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
+    it('Test scenario #4', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        logout()
+        login(userName, password)
+        updateAPost()
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
     it('Test scenario #5', () => {
         createAPost(postTitle, 'Pets are awesome')
         logout()
@@ -61,5 +108,116 @@ describe('Ghost testing', () => {
             logout()
             checkIfPostExists(postUrl)
         })
-    });    
+    });
+
+    it('Test scenario #10', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        logout()
+        login(userName, password)
+        
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
+    it('Test scenario #11', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        updateAPost()
+        deleteAPost()
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostDoesNotExist(postUrl)
+        })
+    });
+
+    it('Test scenario #12', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        updateAPost()
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
+    it('Test scenario #13', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        updateAPost()
+        createAPost('A new post 2', 'A new description')
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
+    it('Test scenario #14', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        deleteAPost()
+        createAPost('A new post 2', 'A new description')
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
+    it('Test scenario #15', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        updateAPost()
+        deleteAPost()
+        
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostDoesNotExist(postUrl)
+        })
+    });
+
+    it('Test scenario #16', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        deleteAPost()
+        
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostDoesNotExist(postUrl)
+        })
+    });
+
+    it('Test scenario #17', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        updateAPost()
+        deleteAPost()
+        
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostDoesNotExist(postUrl)
+        })
+    });
+
+    it('Test scenario #18', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
+    it('Test scenario #19', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        updateAPost()
+        
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostExists(postUrl)
+        })
+    });
+
+    it('Test scenario #20', () => {
+        createAPost(postTitle, 'Pets are awesome')
+        deleteAPost()
+        
+        cy.get('@postUrl').then((postUrl) => {
+            logout()
+            checkIfPostDoesNotExist(postUrl)
+        })
+    });
 });
